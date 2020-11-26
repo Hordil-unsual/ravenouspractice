7686dde73ecc5e57ae116a36abb754c67f216bf6
Hordil <fadildairo@gmail.com>
2020-11-26 01:16:02 +0100

    Api call to Yelp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
-const business = {
+/*const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'Fadil Pizzeria',
   address: '1010 Paddington Way',
@@ -14,13 +15,29 @@ const business = {
   rating: 4.5,
   reviewCount: 90
 };
-
-const businesses = [business, business, business, business, business, business];
+ ***we don't need it since we are making a HTTP request
+const businesses = [
+  business, 
+  business, 
+  business, 
+  business, 
+  business, 
+  business
+];*/
 
 class App extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    businesses: [],
+  };
 
+  this.searchYelp = this.searchYelp.bind(this);
+}
   searchYelp(term, location, sortBy) {
-    console.log(`You are searching for ${term}, ${location}, ${sortBy}`);
+    Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+      this.setState( {businesses: businesses });
+    });
   }
 
   render() {
@@ -28,7 +45,7 @@ class App extends React.Component {
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar searchYelp={this.searchYelp} />
-      <BusinessList businesses= {businesses} /> 
+      <BusinessList businesses= {this.state.businesses} /> 
     </div>
     );
   }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -72,7 +72,7 @@ class SearchBar extends React.Component {
             <input placeholder="Where?" onChange={this.handleLocationChange}/>
           </div>
           <div className="SearchBar-submit">
-            <a href="www.#.com" onClick= {this.handleSearch}>let's Go</a>
+            <a onClick= {this.handleSearch}>let's Go</a>
           </div>
         </div>
         );
